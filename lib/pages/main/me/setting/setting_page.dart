91ad892eac1932172c7a672bce2c886f1efef0aa
dzenm <dinzhenyan1997@126.com>
2023-08-04 16:03:56 +0800

    1.优化代码

--- lib/pages/main/me/setting/setting_page.dart
+++ lib/pages/main/me/setting/setting_page.dart
@@ -1,7 +1,6 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../base/log/build_config.dart';
