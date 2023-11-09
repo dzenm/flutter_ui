fd9b65b1aafe4c058243b8efcca9406dc0f9d5cb
dzenm <dinzhenyan1997@126.com>
2023-11-09 21:56:44 +0800

    1.增加表字段

--- lib/entities/order_entity.dart
+++ lib/entities/order_entity.dart
@@ -15,13 +15,17 @@ class OrderEntity extends DBBaseEntity {
   String? sendAddress; // 发货地址
   String? sendTime; // 发货时间
   String? receiver; // 收获人
-  String? receivePhone; // 发货人手机号
-  String? receiveAddress; // 发货地址
-  String? receiveTime; // 发货地址
+  String? receivePhone; // 收获人手机号
+  String? receiveAddress; // 收获地址
+  String? receiveTime; // 收获时间
   String? trackingNumber; // 快递单号
+  double freight = 0.0; // 运费
   OrderStatus status = OrderStatus.create; // 订单状态
+  int piece = 0; // 件数
+  int weight = 0; // 重量(kg)
   double estimatedPrice = 0.0; // 预估价格
   double actualPrice = 0.0; // 实际卖出的价格
+  PayMethod payMethod = PayMethod.toBePaid; // 支付方式（默认为0）：0=待支付；1=已支付
   int isDelete = 0; // 是否删除
 
   OrderEntity();
@@ -43,9 +47,13 @@ class OrderEntity extends DBBaseEntity {
     receiveAddress TEXT, 
     receiveTime TEXT, 
     trackingNumber TEXT, 
+    freight DOUBLE, 
     status INTEGER, 
+    piece INTEGER, 
+    weight INTEGER, 
     estimatedPrice DOUBLE, 
     actualPrice DOUBLE, 
+    toBePaid INTEGER, 
     isDelete INTEGER
   );''';
 
@@ -77,4 +85,12 @@ enum OrderStatus {
   receivedShip, // 已收货
   @JsonValue(5)
   finish, // 已完成
+}
+
+/// 支付方式
+enum PayMethod {
+  @JsonValue(0)
+  toBePaid, // 待支付
+  @JsonValue(1)
+  paid, // 已支付
 }
