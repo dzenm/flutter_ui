af464445096e90bac9a9a105122027a9cbb3705f
dzenm <dinzhenyan1997@126.com>
2023-11-10 13:38:12 +0800

    1.增加订单页面输入框调整

--- lib/pages/mall/order/add_order_page.dart
+++ lib/pages/mall/order/add_order_page.dart
@@ -47,13 +47,57 @@ class _OrderPageState extends State<AddOrderPage> {
   Widget _buildInputView() {
     AppTheme theme = context.watch<LocalModel>().theme;
     return Column(children: [
-      SingleEditLayout(title: '收货人       ', controller: _usernameController, maxLength: 25),
+      EditLayout(
+        title: const Text(
+          '联系人\u3000',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        hintText: '收件人姓名',
+        color: const Color(0xFFE0E0E0),
+        enabledBorderColor: Colors.transparent,
+        controller: _usernameController,
+        maxLength: 20,
+      ),
       const SizedBox(height: 8),
-      SingleEditLayout(title: '收货地址     ', controller: _addressController, maxLength: 25),
+      EditLayout(
+        title: const Text(
+          '收货地址',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        hintText: '收件人详细地址',
+        color: const Color(0xFFE0E0E0),
+        enabledBorderColor: Colors.transparent,
+        maxLines: 3,
+        keyboardType: TextInputType.multiline,
+        controller: _addressController,
+        maxLength: 30,
+      ),
       const SizedBox(height: 8),
-      SingleEditLayout(title: '收货人手机号码', controller: _phoneController, maxLength: 25),
+      EditLayout(
+        title: const Text(
+          '手机号码',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        hintText: '收件人联系方式',
+        color: const Color(0xFFE0E0E0),
+        enabledBorderColor: Colors.transparent,
+        keyboardType: TextInputType.number,
+        controller: _phoneController,
+        maxLength: 11,
+      ),
       const SizedBox(height: 8),
-      SingleEditLayout(title: '快递单号     ', controller: _trackingNumberController, maxLength: 25),
+      EditLayout(
+        title: const Text(
+          '快递单号',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        hintText: '快递单号',
+        color: const Color(0xFFE0E0E0),
+        enabledBorderColor: Colors.transparent,
+        keyboardType: TextInputType.text,
+        controller: _trackingNumberController,
+        maxLength: 25,
+      ),
       const SizedBox(height: 8),
       TapLayout(
         background: theme.appbar,
@@ -70,11 +114,13 @@ class _OrderPageState extends State<AddOrderPage> {
 
   void _createOrder() {
     OrderEntity order = OrderEntity();
+    order.orderUid = StrUtil.generateUUidString();
     order.receiver = _usernameController.text;
     order.receiveAddress = _addressController.text;
     order.receivePhone = _phoneController.text;
-    order.trackingNumber = _trackingNumberController.text;
-    order.orderUid = _trackingNumberController.text;
+    ProductEntity product = ProductEntity();
+    product.trackingNumber = _trackingNumberController.text;
+    order.products.add(product);
 
     context.read<OrderModel>().updateOrder(order);
     AppRouteDelegate.of(context).pop();
