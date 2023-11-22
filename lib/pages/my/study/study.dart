632dd407d65ecc673a55d72637b621b38ef3a202
dzenm <dinzhenyan1997@126.com>
2023-11-22 16:09:52 +0800

    1.测试Uri

--- lib/pages/my/study/study.dart
+++ lib/pages/my/study/study.dart
@@ -13,6 +13,21 @@ class Study {
     SortStudy.main();
     MathStudy.main();
     ConvertStudy.main();
+    String path = 'https://192.168.0.104/images/app/chat/20231120101757039.mp4?size=100*320';
+    log('path=$path');
+    Uri uri = Uri.parse(path);
+    log('path=${uri.path}');
+    log('pathSegments=${uri.pathSegments}');
+    log('query=${uri.query}');
+    log('queryParameters=${uri.queryParameters}');
+    log('queryParametersAll=${uri.queryParametersAll}');
+    log('data=${uri.data}');
+    log('authority=${uri.authority}');
+    log('fragment=${uri.fragment}');
+    log('userInfo=${uri.userInfo}');
+    log('scheme=${uri.scheme}');
+    log('port=${uri.port}');
+    log('origin=${uri.origin}');
   }
 
   static void log(String msg) {
