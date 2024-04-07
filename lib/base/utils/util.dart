1b19d57b643c12b58896e78043612971e045ed30
dzenm <dinzhenyan1997@126.com>
2024-04-07 16:44:47 +0800

    调整文件结构

--- lib/base/utils/util.dart
+++ lib/base/utils/util.dart
@@ -4,6 +4,9 @@
 library util;
 
 export 'date_util.dart';
+export 'desktop/hotkey_util.dart';
+export 'desktop/open_desktop_app_util.dart';
+export 'desktop/registry_util.dart';
 export 'device_util.dart';
 export 'file_util.dart';
 export 'notification_util.dart';
