70a8d3e206ebbaaf76a9135179ca5ca609ea2620
dzenm <dinzhenyan1997@126.com>
2024-05-08 16:57:54 +0800

    分离参数

--- lib/base/config/handle_error.dart
+++ lib/base/config/handle_error.dart
@@ -15,23 +15,21 @@ typedef HandleMsg = void Function(String message);
 /// 全局处理错误信息
 /// HandleError().catchFlutterError(() {
 ///   runMockApp(AppPage(child: _initApp()));
-/// }, handleMsg: (message) async {
-///   String logFileName = 'crash_${DateTime.now()}.log';
-///   await FileUtil()save(logFileName, message, dir: 'crash').then((String? filePath) async {});
-/// });
+/// }, config: MessageConfig(handleMsg: (message) async {
+///     String logFileName = 'crash_${DateTime.now()}.log';
+///     await FileUtil()save(logFileName, message, dir: 'crash').then((String? filePath) async {});
+///   }),
+/// )
 class HandleError {
   /// 捕获flutter运行时的错误
   Future catchFlutterError(
     Function runApp, {
-    HandleMsg? handleMsg,
-    bool showPackageInfo = true,
-    bool showDeviceInfo = false,
-    bool showStackInfo = true,
-    bool showLogInConsole = true,
+    MessageConfig? config,
   }) async {
+    MessageConfig messageConfig = config ?? MessageConfig();
     // 进行异常捕获并输出
     FlutterError.onError = (FlutterErrorDetails details) async {
-      if (handleMsg == null) {
+      if (messageConfig.handleMsg == null) {
         // 3.3之后使用
         // FlutterError.presentError(details);
         FlutterError.dumpErrorToConsole(details);
@@ -64,7 +62,7 @@ class HandleError {
     // flutter sdk 3.3之后使用下面的方式捕获异常
     PlatformDispatcher.instance.onError = (dynamic error, StackTrace stackTrace) {
       // 处理异常信息
-      _handleMessage(error, stackTrace, handleMsg, showPackageInfo, showDeviceInfo, showStackInfo, showLogInConsole);
+      _handleMessage(error, stackTrace, messageConfig);
       return true;
     };
     // 进入App前先初始化信息
@@ -83,24 +81,16 @@ class HandleError {
   /// 处理信息
   /// [error] 错误文本信息
   /// [stackTrace] 错误的堆栈信息
-  /// [handleMsg] 自定义处理错误信息
-  /// [showPackageInfo] 是否显示包相关信息
-  /// [showDeviceInfo] 是否显示设备相关信息
-  /// [showStackInfo] 是否显示堆栈信息
-  /// [showLogInConsole] 是否在控制台打印错误信息
+  /// [config] 信息输出配置
   void _handleMessage(
     dynamic error,
     StackTrace stackTrace,
-    HandleMsg? handleMsg,
-    bool showPackageInfo,
-    bool showDeviceInfo,
-    bool showStackInfo,
-    bool showLogInConsole,
+    MessageConfig config,
   ) {
     StringBuffer sb = StringBuffer();
     const String interval = '  '; // 缩进的距离
     // 打印日志
-    void log(dynamic msg) => showLogInConsole ? Log.e(msg, tag: 'HandleError') : null;
+    void log(dynamic msg) => config.showLogInConsole ? Log.e(msg, tag: 'HandleError') : null;
     // 处理单条信息
     void handleSingleMessage(String message, {bool needLog = true}) {
       if (message.isEmpty || message == '\n') return; // 过滤空白信息和换行符
@@ -115,21 +105,21 @@ class HandleError {
     log('╚══════════════════════════════════════════════════════════════════════════════════════════════════╝');
 
     // 包名信息
-    if (showPackageInfo) {
+    if (config.showPackageInfo) {
       log('╔═════════════════════════════════════════ Package Info ════════════════════════════════════════════');
     }
     Map<String, dynamic> packageInfo = getPackageInfo();
     for (var key in packageInfo.keys) {
-      handleSingleMessage('$key: ${packageInfo[key]}', needLog: showPackageInfo);
+      handleSingleMessage('$key: ${packageInfo[key]}', needLog: config.showPackageInfo);
     }
 
     // 设备信息
-    if (showDeviceInfo) {
+    if (config.showDeviceInfo) {
       log('║═════════════════════════════════════════ Device Info ═════════════════════════════════════════════');
     }
     Map<String, dynamic> devicesInfo = getDeviceInfo();
     for (var key in devicesInfo.keys) {
-      handleSingleMessage('$key: ${devicesInfo[key]}', needLog: showDeviceInfo);
+      handleSingleMessage('$key: ${devicesInfo[key]}', needLog: config.showDeviceInfo);
     }
 
     // 异常信息
@@ -137,19 +127,19 @@ class HandleError {
     handleSingleMessage('$error');
 
     // 异常信息栈
-    if (showStackInfo) {
+    List<String> stackInfo = stackTrace.toString().split('\n');
+    if (config.showStackInfo && stackInfo.isNotEmpty) {
       log('║═════════════════════════════════════════ Stack Trace ═════════════════════════════════════════════');
     }
-    List<String> stackInfo = stackTrace.toString().split('\n');
     for (var msg in stackInfo) {
-      handleSingleMessage(msg, needLog: showStackInfo);
+      handleSingleMessage(msg, needLog: config.showStackInfo);
     }
 
     log('╚═══════════════════════════════════════════════════════════════════════════════════════════════════');
 
-    if (handleMsg == null) return;
+    if (config.handleMsg == null) return;
     // 处理信息，可以保存文本信息为文件或者上传至服务器
-    handleMsg(sb.toString());
+    config.handleMsg!(sb.toString());
   }
 
   /// 获取APP信息
@@ -198,4 +188,19 @@ class HandleError {
     }
     return map;
   }
+}
+
+class MessageConfig {
+  HandleMsg? handleMsg;  // 自定义处理错误信息
+  bool showPackageInfo;  // 是否显示包相关信息
+  bool showDeviceInfo;   // 是否显示设备相关信息
+  bool showStackInfo;    // 是否显示堆栈信息
+  bool showLogInConsole; // 是否在控制台打印错误信息
+  MessageConfig({
+    this.handleMsg,
+    this.showPackageInfo = true,
+    this.showDeviceInfo = false,
+    this.showStackInfo = true,
+    this.showLogInConsole = true,
+  });
 }
