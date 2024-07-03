56ad559508f458fa55578da8f56c6982d2cdd999
dzenm <dinzhenyan1997@126.com>
2024-07-03 10:32:01 +0800

    增加iOS路径测试代码

--- lib/pages/study/study_main.dart
+++ lib/pages/study/study_main.dart
@@ -16,27 +16,32 @@ class StudyMain {
 
 
   static Future<void> _pathProviderTest() async {
-    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library/Caches
     // Android：/data/user/0/com.dzenm.flutter_ui/cache
+    // iOS：/var/mobile/Containers/Data/Application/3A0617A0-0774-401A-98E8-4713AF0CDCB6/Library/Caches
+    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library/Caches
     Directory? temp = await getTemporaryDirectory();
     _log('获取文件路径：temp=${temp.path}');
 
-    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library/Application Support/com.dzenm.flutterUi
     // Android：/data/user/0/com.dzenm.flutter_ui/files
+    // iOS：/var/mobile/Containers/Data/Application/3A0617A0-0774-401A-98E8-4713AF0CDCB6/Library/Application Support
+    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library/Application Support/com.dzenm.flutterUi
     Directory? support = await getApplicationSupportDirectory();
     _log('获取文件路径：support=${support.path}');
 
-    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Documents
     // Android：/data/user/0/com.dzenm.flutter_ui/app_flutter
+    // iOS：/var/mobile/Containers/Data/Application/3A0617A0-0774-401A-98E8-4713AF0CDCB6/Documents
+    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Documents
     Directory? document = await getApplicationDocumentsDirectory();
     _log('获取文件路径：document=${document.path}');
 
-    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library/Caches/com.dzenm.flutterUi
     // Android：/data/user/0/com.dzenm.flutter_ui/cache
+    // iOS：/var/mobile/Containers/Data/Application/3A0617A0-0774-401A-98E8-4713AF0CDCB6/Library/Caches
+    // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library/Caches/com.dzenm.flutterUi
     Directory? cache = await getApplicationCacheDirectory();
     _log('获取文件路径：cache=${cache.path}');
 
     if (!BuildConfig.isAndroid) {
+      // iOS：/var/mobile/Containers/Data/Application/3A0617A0-0774-401A-98E8-4713AF0CDCB6/Library
       // macOS：/Users/a0010/Library/Containers/com.dzenm.flutterUi/Data/Library
       Directory? library = await getLibraryDirectory();
       _log('获取文件路径：library=${library.path}');
