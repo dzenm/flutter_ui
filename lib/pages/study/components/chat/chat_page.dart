54069bd0c680a21d1022529452adbad706421d13
dzenm <dinzhenyan1997@126.com>
2025-04-25 09:24:34 +0800

    优化聊天列表

--- lib/pages/study/components/chat/chat_page.dart
+++ lib/pages/study/components/chat/chat_page.dart
@@ -8,7 +8,6 @@ import 'package:provider/provider.dart';
 
 import 'chat_item_widget.dart';
 import 'chat_model.dart';
-import 'chat_unread_tip_view.dart';
 
 ///
 /// Created by a0010 on 2023/10/30 16:27
@@ -21,48 +20,22 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final ScrollController _scrollerController = ScrollController();
-  late ListObserverController _controller;
-  late ChatScrollObserver _chatObserver;
-
-  ValueNotifier<int> unreadMsgCount = ValueNotifier<int>(0);
-  bool needIncrementUnreadMsgCount = false;
-  bool editViewReadOnly = false;
-  TextEditingController editViewController = TextEditingController();
+  bool isShowClassicHeaderAndFooter = false;
   BuildContext? pageOverlayContext;
   final LayerLink layerLink = LayerLink();
-  bool isShowClassicHeaderAndFooter = false;
+  bool editViewReadOnly = false;
+  TextEditingController editViewController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
 
     Provider.of<StudyModel>(context, listen: false).initModels();
-    _scrollerController.addListener(scrollControllerListener);
-    _controller = ListObserverController(controller: _scrollerController)..cacheJumpIndexOffset = false;
-    _chatObserver = ChatScrollObserver(_controller)
-      ..fixedPositionOffset = 5
-      ..toRebuildScrollViewCallback = () {
-        setState(() {});
-      }
-      ..onHandlePositionResultCallback = (result) {
-        if (!needIncrementUnreadMsgCount) return;
-        switch (result.type) {
-          case ChatScrollObserverHandlePositionType.keepPosition:
-            updateUnreadMsgCount(changeCount: result.changeCount);
-            break;
-          case ChatScrollObserverHandlePositionType.none:
-            updateUnreadMsgCount(isReset: true);
-            break;
-        }
-      };
-
     Future.delayed(const Duration(seconds: 1), addUnreadTipView);
   }
 
   @override
   void dispose() {
-    _controller.controller?.dispose();
     editViewController.dispose();
     super.dispose();
   }
@@ -93,21 +66,17 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Widget _buildPageOverlay() {
-    return Overlay(initialEntries: [
-      OverlayEntry(
-        builder: (context) {
-          pageOverlayContext = context;
-          return Container();
-        },
-      )
-    ]);
-  }
-
   Widget _buildBody() {
+    var list = Provider.of<StudyModel>(context).chatModels;
+    Widget child;
+    if (list.isEmpty) {
+      child = Container();
+    } else {
+      child = const _MessageListView();
+    }
     Widget resultWidget = Column(
       children: [
-        Expanded(child: _buildListView()),
+        Expanded(child: child),
         CompositedTransformTarget(
           link: layerLink,
           child: Container(),
@@ -121,6 +90,17 @@ class _ChatPageState extends State<ChatPage> {
       _buildPageOverlay(),
     ]);
     return resultWidget;
+  }
+
+  Widget _buildPageOverlay() {
+    return Overlay(initialEntries: [
+      OverlayEntry(
+        builder: (context) {
+          pageOverlayContext = context;
+          return Container();
+        },
+      )
+    ]);
   }
 
   Widget _buildEditView() {
@@ -167,29 +147,132 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Widget _buildUnreadTipView() {
-    return ValueListenableBuilder<int>(
-      builder: (context, value, child) {
-        return ChatUnreadTipView(
-          unreadMsgCount: unreadMsgCount.value,
-          onTap: () {
-            _scrollerController.animateTo(
-              0,
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-            );
+  void _sendMessage() {
+    editViewController.text = '';
+    Provider.of<StudyModel>(context, listen: false).insert();
+    setState(() {});
+  }
+
+  addUnreadTipView() {
+    Overlay.of(pageOverlayContext!).insert(OverlayEntry(
+      builder: (BuildContext context) => UnconstrainedBox(
+        child: CompositedTransformFollower(
+          link: layerLink,
+          followerAnchor: Alignment.bottomRight,
+          targetAnchor: Alignment.topRight,
+          offset: const Offset(-20, 0),
+          child: Material(
+            type: MaterialType.transparency,
+            color: Colors.green,
+            // child: _buildUnreadTipView(),
+          ),
+        ),
+      ),
+    ));
+  }
+
+// Widget _buildUnreadTipView() {
+//   return ValueListenableBuilder<int>(
+//     builder: (context, value, child) {
+//       return ChatUnreadTipView(
+//         unreadMsgCount: unreadMsgCount.value,
+//         onTap: () {
+//           _scrollerController.animateTo(
+//             0,
+//             duration: const Duration(milliseconds: 300),
+//             curve: Curves.easeInOut,
+//           );
+//           updateUnreadMsgCount(isReset: true);
+//         },
+//       );
+//     },
+//     valueListenable: unreadMsgCount,
+//   );
+// }
+}
+
+class _MessageListView extends StatefulWidget {
+  final ScrollPhysics? physics;
+
+  const _MessageListView({
+    super.key,
+    this.physics,
+  });
+
+  @override
+  State<_MessageListView> createState() => _MessageListViewState();
+}
+
+class _MessageListViewState extends State<_MessageListView> with WidgetsBindingObserver {
+  final ScrollController _scrollerController = ScrollController();
+  late ListObserverController _controller;
+  late ChatScrollObserver _chatObserver;
+
+  ValueNotifier<int> unreadMsgCount = ValueNotifier<int>(0);
+  bool needIncrementUnreadMsgCount = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
+    _scrollerController.addListener(scrollControllerListener);
+    _controller = ListObserverController(controller: _scrollerController)..cacheJumpIndexOffset = false;
+    _chatObserver = ChatScrollObserver(_controller)
+      ..fixedPositionOffset = 5
+      ..toRebuildScrollViewCallback = () {
+        setState(() {});
+      }
+      ..onHandlePositionResultCallback = (result) {
+        if (!needIncrementUnreadMsgCount) return;
+        switch (result.type) {
+          case ChatScrollObserverHandlePositionType.keepPosition:
+            updateUnreadMsgCount(changeCount: result.changeCount);
+            break;
+          case ChatScrollObserverHandlePositionType.none:
             updateUnreadMsgCount(isReset: true);
-          },
-        );
-      },
-      valueListenable: unreadMsgCount,
-    );
+            break;
+        }
+      };
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _controller.controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeMetrics() {
+    super.didChangeMetrics();
+    // Update shrinkWrap in real time as the keyboard pops up or closes.
+    _chatObserver.observeSwitchShrinkWrap();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        if (MediaQuery.of(context).viewInsets.bottom == 0) {
+          // Keyboard closes
+        } else {
+          // Keyboard pops up
+          _scrollerController.jumpTo(0);
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildListView(physics: widget.physics);
   }
 
   Widget _buildListView({ScrollPhysics? physics}) {
     return Selector0<List<ChatModel>>(
       selector: (context) => Provider.of<StudyModel>(context).chatModels,
       builder: (c, chatModels, w) {
+        if (chatModels.isEmpty) {
+          return Container();
+        }
         return ChatView(
           scrollerController: _scrollerController,
           controller: _controller,
@@ -215,48 +298,11 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  addUnreadTipView() {
-    Overlay.of(pageOverlayContext!).insert(OverlayEntry(
-      builder: (BuildContext context) => UnconstrainedBox(
-        child: CompositedTransformFollower(
-          link: layerLink,
-          followerAnchor: Alignment.bottomRight,
-          targetAnchor: Alignment.topRight,
-          offset: const Offset(-20, 0),
-          child: Material(
-            type: MaterialType.transparency,
-            // color: Colors.green,
-            child: _buildUnreadTipView(),
-          ),
-        ),
-      ),
-    ));
-  }
-
-  void _sendMessage() {
-    _chatObserver.standby(changeCount: 1);
-    editViewController.text = '';
-    setState(() {
-      needIncrementUnreadMsgCount = true;
-      Provider.of<StudyModel>(context, listen: false).insert();
-    });
-  }
-
-  addMessage(int count) {
-    _chatObserver.standby(changeCount: count);
-    setState(() {
-      needIncrementUnreadMsgCount = true;
-      for (var i = 0; i < count; i++) {
-        Provider.of<StudyModel>(context, listen: false).insert();
-      }
-    });
-  }
-
   updateUnreadMsgCount({
     bool isReset = false,
     int changeCount = 1,
   }) {
-    needIncrementUnreadMsgCount = false;
+    // needIncrementUnreadMsgCount = false;
     if (isReset) {
       unreadMsgCount.value = 0;
     } else {
@@ -270,11 +316,15 @@ class _ChatPageState extends State<ChatPage> {
     }
   }
 
+  void _sendMessage() {
+    _chatObserver.standby(changeCount: 1);
+    needIncrementUnreadMsgCount = true;
+  }
+
   int genInt({int min = 0, int max = 100}) {
     var x = Random().nextInt(max) + min;
     return x.floor();
   }
-
 }
 
 /// 发送消息快捷键创建 Intent
